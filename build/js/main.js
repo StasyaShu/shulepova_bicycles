fb637d462cec35b328a7059c8bf0578fa1c3289f
StasyaShu <stanislava.semenova@gmail.com>
2021-11-09 22:29:53 +0300

    написан js для страницы

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,57 @@
-import {scrollToAnchor} from './scroll.js';
-import {showNav} from './mobile-menu.js';
-import {DATA_FORM_INPUTS} from './data.js';
+const ANCHORS = document.querySelectorAll('a[href*="#"]');
+const SITE_NAVIGATION = document.querySelector('.main-nav');
+const SITE_NAVIGATION_TOGGLE = document.querySelector('.main-nav__toggle');
+const INPUT_NAME = document.getElementById('name-id');
+const INPUT_TEL = document.getElementById('tel-id');
 
-scrollToAnchor();
+
+const showNav = () => {
+  SITE_NAVIGATION_TOGGLE.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    SITE_NAVIGATION.classList.toggle('main-nav--opened');
+    SITE_NAVIGATION.classList.toggle('main-nav--closed');
+  });
+};
+
 showNav();
 
-DATA_FORM_INPUTS.addEventListener('invalid', function () {
-  if (DATA_FORM_INPUTS.validity.tooShort) {
-    DATA_FORM_INPUTS.setCustomValidity('Введите пожалуйста минимум 2 символа');
-  } else if (DATA_FORM_INPUTS.validity.tooLong) {
-    DATA_FORM_INPUTS.setCustomValidity('Значение не должно превышать 25-ти символов');
-  } else if (DATA_FORM_INPUTS.validity.valueMissing) {
-    DATA_FORM_INPUTS.setCustomValidity('Введите пожалуйста данные');
-  } else if (DATA_FORM_INPUTS.patternMismatch) {
-    DATA_FORM_INPUTS.setCustomValidity('Номер телефона должен быть формата: +7-ххх-ххх-хх-хх');
+const scrollToAnchor = () => {
+  for (let anchor of ANCHORS) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const blockID = anchor.getAttribute('href').substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+};
+
+scrollToAnchor();
+
+
+INPUT_NAME.addEventListener('invalid', () => {
+  if (INPUT_NAME.validity.tooShort) {
+    INPUT_NAME.setCustomValidity('Введите пожалуйста минимум 2 символа');
+  } else if (INPUT_NAME.validity.tooLong) {
+    INPUT_NAME.setCustomValidity('Значение не должно превышать 25-ти символов');
+  } else if (INPUT_NAME.validity.valueMissing) {
+    INPUT_NAME.setCustomValidity('Введите пожалуйста Ваше имя');
   } else {
-    DATA_FORM_INPUTS.setCustomValidity('');
+    INPUT_NAME.setCustomValidity('');
+  }
+});
+
+INPUT_TEL.addEventListener('invalid', () => {
+  console.log();
+  if (INPUT_TEL.validity.patternMismatch) {
+    INPUT_TEL.setCustomValidity('Номер телефона должен быть в формате: +7-ххх-ххх-хх-хх');
+  } else if (INPUT_TEL.validity.valueMissing) {
+    INPUT_TEL.setCustomValidity('Введите пожалуйста номер телефона');
+  } else {
+    INPUT_TEL.setCustomValidity('');
   }
 });
--- source/js/data.js
+++
@@ -1,6 +0,0 @@
-const ANCHORS = document.querySelectorAll('a[href*="#"]');
-const SITE_NAVIGATION = document.querySelector('.main-nav');
-const SITE_NAVIGATION_TOGGLE = document.querySelector('.main-nav__toggle');
-const DATA_FORM_INPUTS = document.querySelectorAll('input');
-
-export {ANCHORS, SITE_NAVIGATION, SITE_NAVIGATION_TOGGLE, DATA_FORM_INPUTS};
