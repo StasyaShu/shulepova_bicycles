d8a61b4ac19652a301b0d879bae60edea42a92a6
StasyaShu <stanislava.semenova@gmail.com>
2021-11-09 18:16:48 +0300

    validation

--- source/js/data.js
+++ source/js/data.js
@@ -1,7 +1,6 @@
 const ANCHORS = document.querySelectorAll('a[href*="#"]');
 const SITE_NAVIGATION = document.querySelector('.main-nav');
 const SITE_NAVIGATION_TOGGLE = document.querySelector('.main-nav__toggle');
-const INPUT_NAME = document.getElementById('name-id');
-const INPUT_TEL = document.getElementById('tel-id');
+const DATA_FORM_INPUTS = document.querySelectorAll('input');
 
-export {ANCHORS, SITE_NAVIGATION, SITE_NAVIGATION_TOGGLE, INPUT_NAME, INPUT_TEL};
+export {ANCHORS, SITE_NAVIGATION, SITE_NAVIGATION_TOGGLE, DATA_FORM_INPUTS};
--- source/js/main.js
+++ source/js/main.js
@@ -1,29 +1,20 @@
 import {scrollToAnchor} from './scroll.js';
 import {showNav} from './mobile-menu.js';
-import {INPUT_NAME, INPUT_TEL} from './data.js';
+import {DATA_FORM_INPUTS} from './data.js';
 
 scrollToAnchor();
 showNav();
 
-INPUT_NAME.addEventListener('input', () => {
-  if (INPUT_NAME.validity.tooShort) {
-    INPUT_NAME.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-  } else if (INPUT_NAME.validity.tooLong) {
-    INPUT_NAME.setCustomValidity('Имя не должно превышать 25-ти символов');
-  } else if (INPUT_NAME.validity.valueMissing) {
-    INPUT_NAME.setCustomValidity('Введите пожалуйста Ваше имя');
+DATA_FORM_INPUTS.addEventListener('invalid', function () {
+  if (DATA_FORM_INPUTS.validity.tooShort) {
+    DATA_FORM_INPUTS.setCustomValidity('Введите пожалуйста минимум 2 символа');
+  } else if (DATA_FORM_INPUTS.validity.tooLong) {
+    DATA_FORM_INPUTS.setCustomValidity('Значение не должно превышать 25-ти символов');
+  } else if (DATA_FORM_INPUTS.validity.valueMissing) {
+    DATA_FORM_INPUTS.setCustomValidity('Введите пожалуйста данные');
+  } else if (DATA_FORM_INPUTS.patternMismatch) {
+    DATA_FORM_INPUTS.setCustomValidity('Номер телефона должен быть формата: +7-ххх-ххх-хх-хх');
   } else {
-    INPUT_NAME.setCustomValidity('');
+    DATA_FORM_INPUTS.setCustomValidity('');
   }
 });
-
-INPUT_TEL.addEventListener('input', () => {
-  if (INPUT_TEL.validity.valueMissing) {
-    INPUT_TEL.setCustomValidity('Введите пожалуйста Ваш номер телефона');
-  } else if (INPUT_TEL.patternMismatch) {
-    INPUT_TEL.setCustomValidity('Номер телефона должен быть формата: +7-ххх-ххх-хх-хх')
-  } else {
-    INPUT_TEL.setCustomValidity('');
-  }
-});
-
